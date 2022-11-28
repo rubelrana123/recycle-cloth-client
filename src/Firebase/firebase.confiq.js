9b17be66a667261ba7a363fadd776b47a0404487
Md Rubel Rana <96787688+rubelrana123@users.noreply.github.com>
2022-11-28 08:16:04 +0600

    firebase hide secret key

--- src/Firebase/firebase.confiq.js
+++ src/Firebase/firebase.confiq.js
@@ -1,25 +1,15 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCWrWDOUFk-0w3LeTOGeAIKALd04sqwJN8",
-  authDomain: "recycle-clothh.firebaseapp.com",
-  projectId: "recycle-clothh",
-  storageBucket: "recycle-clothh.appspot.com",
-  messagingSenderId: "937627916947",
-  appId: "1:937627916947:web:1116b1a7bbf42b343b8b66"
+  apiKey:process.env.REACT_APP_apiKey ,
+  authDomain:process.env.REACT_APP_authDomain ,
+  projectId:process.env.REACT_APP_projectId ,
+  storageBucket:process.env.REACT_APP_storageBucket ,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId ,
+  appId:process.env.REACT_APP_appId 
 };
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_apiKey,
-//   authDomain: process.env.REACT_APP_authDomain,  
-//   projectId: process.env.REACT_APP_projectId,   
-//   storageBucket: process.env.REACT_APP_storageBucket, 
-//   messagingSenderId: process.env.REACT_APP_messagingSenderId,  
-//   appId: process.env.REACT_APP_appId   
-// };
+
 
 // Initialize Firebase
 export  const app = initializeApp(firebaseConfig);
